e5d30bd70becabd03023825d38b44a1b87495774
MasonQiao <119145677+MasonQiao@users.noreply.github.com>
2024-04-16 18:23:23 -0500

    timer is frac instead of float

--- 24.rkt
+++ 24.rkt
@@ -28,14 +28,14 @@
                  [(> (p-mt m) 0) (overlay/align "middle" "top" (text "Time ran out. Mouse wins." 20 "black") (place-image mouseimage (p-mx m) (p-my m) (place-image catimage (p-cx m) (p-cy m) floorimage)))]
                  [else (overlay/align "middle" "top" (text (string-append "Time Left: " (number->string (p-t m))) 20 "black") (place-image mouseimage (p-mx m) (p-my m) (place-image catimage (p-cx m) (p-cy m) floorimage)))]))
 
-(define (th m) (if (<= (p-t m) 0) (mwins m) (if (< (distance (p-cx m) (p-cy m) (p-mx m) (p-my m)) 100) (cwins m) m)))
-(define (cwins m) (make-p (p-m m) 500 100 1400 100 (+ (p-cs m) (score scorelist)) (p-ms m) 30 5 0))
-(define (mwins m) (make-p (p-m m) 500 100 1400 100 (p-cs m) (+ (p-ms m) (score scorelist)) 30 0 5))
+(define (th m) (if (<= (p-t m) 0) (mwins m) (if (< (distance (p-cx m) (p-cy m) (p-mx m) (p-my m)) 100) (cwins m) (make-p (p-m m) (p-cx m) (p-cy m) (p-mx m) (p-my m) (p-cs m) (p-ms m) (max 0 (- (p-t m) 0.05)) (max 0 (- (p-ct m) 0.05)) (max 0 (- (p-mt m) 0.05))))))
+(define (cwins m) (make-p (p-m m) 500 100 1400 100 (+ (p-cs m) (score scorelist)) (p-ms m) 20 5 0))
+(define (mwins m) (make-p (p-m m) 500 100 1400 100 (p-cs m) (+ (p-ms m) (score scorelist)) 20 0 5))
 
 (define (kh m key) (if (string=? (p-m m) "mouse") m
                        (cond [(key=? key "w") (make-package (make-p "cat" (p-cx m) (max 0 (- (p-cy m) 10)) (p-mx m) (p-my m) (p-cs m) (p-ms m) (p-t m) (p-ct m) (p-mt m)) (make-posm "m" (p-cx m) (max 0 (- (p-cy m) 10))))]
                              [(key=? key "a") (make-package (make-p "cat" (max 0 (- (p-cx m) 10)) (p-cy m) (p-mx m) (p-my m) (p-cs m) (p-ms m) (p-t m) (p-ct m) (p-mt m)) (make-posm "m" (max 0 (- (p-cx m) 10)) (p-cy m)))]
-                             [(key=? key "s") (make-package (make-p "cat" (p-cx m) (min 1500 (+ (p-cy m) 10)) (p-mx m) (p-my m) (p-cs m) (p-ms m) (p-t m) (p-ct m) (p-mt m)) (make-posm "m" (p-cx m) (min 1500 (+ (p-cy m) 10))))]
+                             [(key=? key "s") (make-package (make-p "cat" (p-cx m) (min 990 (+ (p-cy m) 10)) (p-mx m) (p-my m) (p-cs m) (p-ms m) (p-t m) (p-ct m) (p-mt m)) (make-posm "m" (p-cx m) (min 1500 (+ (p-cy m) 10))))]
                              [(key=? key "d") (make-package (make-p "cat" (min 1500 (+ (p-cx m) 10)) (p-cy m) (p-mx m) (p-my m) (p-cs m) (p-ms m) (p-t m) (p-ct m) (p-mt m)) (make-posm "m" (min 1500 (+ (p-cx m) 10)) (p-cy m)))]
                              [else m])))
 
@@ -44,15 +44,24 @@
         [(and (string=? (posm-to msg) "m") (string=? (p-m m) "mouse")) (make-p "mouse" (posm-x msg) (posm-y msg) (p-mx m) (p-my m) (p-cs m) (p-ms m) (p-t m) (p-ct m) (p-mt m))]
         [else m]))
 
+(define (bx a) (max 0 (min 1500 a)))
+(define (by a) (max 0 (min 990 a)))
+
+(define (mh m x y event) (if (string=? (p-m m) "cat") m
+                (make-package (make-p "mouse" (p-cx m) (p-cy m) (bx (+ (* 0.99 (p-mx m)) (* 0.01 x))) (by (+ (* 0.99 (p-my m)) (* 0.01 y))) (p-cs m) (p-ms m) (p-t m) (p-ct m) (p-mt m)) (make-posm "c" (bx (+ (* 0.9 (p-mx m)) (* 0.1 x))) (by (+ (* 0.9 (p-my m)) (* 0.1 y)))))))
+
+
+
 (define (start-bang title initial)
   (big-bang initial
     (name title)
     (register LOCALHOST) ;; see 08 Server
     (to-draw dh1)
     (on-key kh)
-    (on-tick th)
-    (on-receive rh)))
+    (on-tick th 0.05)
+    (on-receive rh)
+    (on-mouse mh)))
 
 (launch-many-worlds
- (start-bang "cat" (make-p "cat" 500 100 1400 100 0 0 30 0 0))
- (start-bang "mouse" (make-p "mouse" 500 100 1400 100 0 0 30 0 0)))
+ (start-bang "cat" (make-p "cat" 500 100 1400 100 0 0 20 0 0))
+ (start-bang "mouse" (make-p "mouse" 500 100 1400 100 0 0 20 0 0)))
